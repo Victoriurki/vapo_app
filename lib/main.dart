fdcc5e22089d4b3c0f6aea010b278767883b7654
matheusmei <71150878+matheusmei@users.noreply.github.com>
2022-04-05 11:01:38 -0300

    Prepare the main to login function

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:vapo_app/AppTheme/app_theme.dart';
 import 'package:vapo_app/Login%20Feature/login_page.dart';
-
 import 'app_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp();
+
   runApp(const AppWidget());
 }
 
